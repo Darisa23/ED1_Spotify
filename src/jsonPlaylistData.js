0a383931a2ecf09043219854e75b5ec68702f558
Dariana <sdariana@uninorte.edu.co>
2025-04-05 01:13:12 -0500

    modulación

--- src/jsonPlaylistData.js
+++ src/jsonPlaylistData.js
@@ -1,46 +1,7 @@
-import axios from 'axios';
 //importamos módulo de File System de Node.js. Sirve para leer, escribir, modificar o eliminar archivos
 import fs from 'fs'; 
-import getToken from './getToken.js';
-
-//guardamos el id de la playlist que queramos
-const PLAYLIST_ID = '3mn4kXt07PEGZFR46h3HhN';
-
-const getTracksFromPlaylist = async (token) => {
-    //usamos el endpoint de la playlist
-  const url = `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks?limit=50`;
-
-  const res = await axios.get(url, {
-    headers: { Authorization: 'Bearer ' + token }
-  });
-//extraemos los datos de los nombres de las canciones, el artista y el id de la canción
-  const tracks = res.data.items.map(item => ({
-    id: item.track.id,
-    name: item.track.name,
-    artist: item.track.artists.map(a => a.name).join(', ')
-  }));
-
-  return tracks;
-};
-
-const getTrackDetails = async (trackIds, token) => {
-//convertimos una lista de IDs en un solo string separado por comas
-  const ids = trackIds.join(',');
-  //usamos el endpoint de GetTrack
-  const url = `https://api.spotify.com/v1/tracks?ids=${ids}`;
-
-  const res = await axios.get(url, {
-    headers: { Authorization: 'Bearer ' + token }
-  });
-//nos retorna los datos esperados
-  return res.data.tracks.map(track => ({
-    id: track.id,
-    name: track.name,
-    artist: track.artists.map(a => a.name).join(', '),
-    popularity: track.popularity,
-    duration_ms: track.duration_ms
-  }));
-};
+import getToken from './utils/getToken.js';
+import { getTracksFromPlaylist, getTrackDetails } from './utils/spotifyUtils.js';
 
 const main = async () => {
     //llamamos el token
